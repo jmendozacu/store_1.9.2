9e6117d18af773cd34ce821fc5f07fa91e148cca
salimmujawar <salim685@gmail.com>
2016-08-09 23:10:41 +0530

    creating a flat with model and adding a entry

---
+++ app/code/local/Packt/Helloworld/Model/Subscription.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* The model
+*/
+class Packt_Helloworld_Model_Subscription extends Mage_Core_Model_Abstract
+{
+	
+	protected	function _construct() {
+
+		$this->_init('helloworld/subscription');		
+	}
+	
+}
